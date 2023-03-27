a4244293a73d70885f2275da19f14dfaa7f616a1
miquart2005 <miquart2005@mail.com>
2023-03-27 07:24:46 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,15 +5,15 @@
 #include "main.h"
 
 int _putchar(char c);
-void reset_to_98(int n);
-int _isdigit(int *a,int *b);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-void prin_array(int *a,int n);
-void print_triangle(char *dest,char *src);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 
 #endif
