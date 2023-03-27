7b6140c2170a2f40d8d7616125bccdad85123d65
miquart2005 <miquart2005@mail.com>
2023-03-27 07:04:54 -0700

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include "main.h"
+
+int _putchar(char c);
+void reset_to_98(int n);
+int _isdigit(int *a,int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void prin_array(int *a,int n);
+void print_triangle(char *dest,char *src);
+
+#endif
